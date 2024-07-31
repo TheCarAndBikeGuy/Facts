bbdbde86531428624fbc4107c49b2fff9248da5b
NOAH SMART <noahsmart48@gmail.com>
2024-07-31 15:10:40 +0100

    sorted

--- app.js
+++ app.js
@@ -3,11 +3,3 @@ window.addEventListener("scroll", function () {
   const nav = this.document.querySelector("nav");
   nav.classList.toggle("Sticky", window.scrollY > 0);
 });
-
-const hamburger = document.querySelector("hamburger");
-const offScreenMenu = document.querySelector("off-screen-menu");
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-})
